e4e4b94910ebabb4310272c19b86ea6ed10ad55d
jjcontreras14 <167757338+jjcontreras14@users.noreply.github.com>
2024-04-30 11:54:14 +0200

    Update ExcelRead.java

--- ExcelRead.java
+++ ExcelRead.java
@@ -3,10 +3,12 @@ package LecturaExcel;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Date;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -15,38 +17,46 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ExcelRead {
 
 	public static void main(String[] args) {
-	
+
 		File archivo = new File("BPM.xlsx");
-		
+
 		try {
 			FileInputStream input = new FileInputStream(archivo);
-			
 			XSSFWorkbook libro = new XSSFWorkbook(input);
-			
 			XSSFSheet hoja = libro.getSheetAt(0);
-			
+
 			Row fila = hoja.getRow(2);
-			
-			Iterator<Cell> columnas = fila.cellIterator();
-			
-			while(columnas.hasNext()) {
-				
-				Cell celda = columnas.next();
-				
-				if(celda.getCellType()== CellType.NUMERIC) {
-					
-					double valor = celda.getNumericCellValue();
-					
-					System.out.println(valor);
+
+			Iterator<Row> filas = hoja.rowIterator();
+			Iterator<Cell> columnas = null;
+			Row filaNOW = null;
+			Cell columnaNOW = null;
+
+			while(filas.hasNext()) {
+				filaNOW=filas.next();
+				columnas=filaNOW.cellIterator();
+				while(columnas.hasNext()){
+					columnaNOW=columnas.next();
+					if(columnaNOW.getCellType()== CellType.NUMERIC) {
+						double valor = columnaNOW.getNumericCellValue();
+						System.out.println(valor);
+					}
+
+					if(columnaNOW.getCellType()== CellType.NUMERIC && DateUtil.isCellDateFormatted(columnaNOW)) {
+						Date fecha = columnaNOW.getDateCellValue();
+						System.out.println(fecha );
+					}
 				}
-				
+
 			}
-			
+			input.close();
+			libro.close();
+
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
 		}
-				
+
 
 	}
 
